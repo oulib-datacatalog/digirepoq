a658b6b48a2355fe1f16399b1e1d267603cb41fb
Tyler Pearson <tdpearson@users.noreply.github.com>
2018-10-30 18:29:45 -0500

    update dependency

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 #ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 setup(name='digirepoq',
-      version='0.1',
+      version='0.2',
       packages= find_packages(),
       install_requires=[
           'celery==3.1.22',
           'bagit==1.5.4',
           'pymongo==3.2.1',
-          'requests==2.13.0',
+          'requests==2.20.0',
           'pillow==3.3.1',
           'PyYAML=3.12',
           'pycurl==7.43.0',
